513e7279070a68d8651950b3c605b83542319c35
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-03 23:36:39 +0100

    remove putler

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -222,7 +222,7 @@ export default function Index() {
                             type="text"
                             id="street"
                             name="street"
-                            placeholder="Putlerova 12"
+                            placeholder="Strašnická 12"
                             className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out"
                           />
                         </div>
