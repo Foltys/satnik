ce12fc64356981629c273b3837621d425011e23c
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-03 09:51:54 +0100

    design changes

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,41 +2,40 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap  flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="/">
-            <img src="red.svg" alt="Šatník Praha" />
+        <div className="container mx-auto flex flex-wrap flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 ml-5 md:ml-0" href="/">
+            <img src="red.svg" className="w-16 h-16 md:w-32 md:h-32" alt="Šatník Praha" />
             <span className="ml-3 text-xl hidden">Šatník</span>
           </a>
-          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <nav className="ml-auto flex flex-wrap items-center text-base justify-center">
             <a className="mr-5 hover:text-gray-900 hidden" href="/">First Link</a>
           </nav>
-          <button className="inline-flex items-center text-[#0A9DBF] font-semibold  border-0 py-2 px-5 focus:outline-none outline outline-offset-2 outline-[#0A9DBF] rounded-full text-base mt-4 md:mt-0">
+          <button className="inline-flex items-center text-[#0A9DBF] font-semibold  border-0 py-2 px-5 focus:outline-none outline outline-offset-2 outline-[#0A9DBF] rounded-full text-base mr-5 hover:outline-[#eb2f06]">
             <svg fill="none" className="w-4 h-4 mr-1" viewBox="0 0 24 24">
               <rect width="24" height="12" fill="#005BBB" />
               <rect width="24" height="12" y="12" fill="#FFD500" />
-            </svg>
-            Український
+            </svg><span className="hidden md:flex">Український</span>
           </button>
         </div>
       </header>
 
       <section className="text-gray-600 body-font relative">
-        <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div className="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-start justify-start relative">
+        <div className="container px-5 py-14 md:py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden sm:mr-10 flex items-start justify-start relative">
 
             <div className="w-full mx-auto">
               <div className="flex flex-col mb-12">
-                <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Kontaktní informace</h1>
+                <h1 className="sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900">Kontaktní informace</h1>
                 <p className="leading-relaxed text-base">Nevim, sem bych něco napsal.</p>
               </div>
               <div className="flex flex-wrap -m-2">
-                <div className="p-2 w-1/2">
+                <div className="p-2 w-full">
                   <div className="relative">
                     <label htmlFor="name" className="leading-7 font-semibold text-sm text-gray-600">Jméno a příjmení</label>
                     <input type="text" id="name" name="name" className="w-full bg-white bg-opacity-80 rounded-xl border border-gray-300 focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                   </div>
                 </div>
-                <div className="p-2 w-1/2">
+                <div className="p-2 w-full">
                   <div className="relative">
                     <label htmlFor="phone" className="leading-7 font-semibold text-sm text-gray-600">Telefon</label>
                     <input type="tel" id="phone" name="phone" className="w-full bg-white bg-opacity-80 rounded-xl border border-gray-300 focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
@@ -48,8 +47,8 @@ export default function Index() {
                     <input type="email" id="email" name="email" className="w-full bg-white bg-opacity-80 rounded-xl border border-gray-300 focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                   </div>
                 </div>
-                <div className="p-2 w-full">
-                  <button className="inline-flex items-center text-[#0A9DBF] font-semibold  border-0 py-2 px-5 focus:outline-none outline  outline-[#0A9DBF] rounded-full text-base mt-2 md:mt-5 hover:bg-[#eb2f06] hover:text-white hover:outline-[#eb2f06]">
+                <div className="p-2 my-10 ml-2 w-full">
+                  <button className="inline-flex items-center text-[#0A9DBF] font-semibold  border-0 py-2 px-5 focus:outline-none outline  outline-[#0A9DBF] rounded-full text-base  hover:bg-[#eb2f06] hover:text-white hover:outline-[#eb2f06]">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                       <path strokeLinecap="round" strokeLinejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
                     </svg>
