c02f690aab058a93017f3f3056678089e89e28ed
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-03 16:37:12 +0100

    first page with state

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,12 @@
 import { useOutletContext } from "remix";
+import { useState } from "react";
 
 export default function Index() {
   const [translate] = useOutletContext();
+  const [delivery, setDelivery] = useState(true);
+  const handleChange = (e) => {
+    delivery ? (setDelivery(false)) : (setDelivery(true));
+  }
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <header className="text-gray-600 body-font sticky top-0 bg-[#F8EBDB] z-10 pb-2">
@@ -67,56 +72,75 @@ export default function Index() {
                 </div>
                 <div className="flex flex-wrap -m-2">
                   <div className="flex mx-3 my-5 font-bold space-x-2 text-[#0A9DBF]">
-                    <button className="border-4 border-[#0A9DBF] focus:bg-[#0A9DBF] rounded-full w-6 h-6 focus:outline-none"></button> <span>Doručení na adresu</span>
-                    <button className="border-4 border-[#0A9DBF] focus:bg-[#0A9DBF] rounded-full w-6 h-6 focus:outline-none"></button> <span>Osobní vyzvednutí</span>
+                    <input className="accent-[#0A9DBF]" type="radio" id="radiodelivery" name="radioselector" value="radiodelivery" checked={delivery == true} onChange={handleChange} />
+                    <label htmlFor="radiodelivery">Doručení na adresu</label>
+                    <input className="accent-[#0A9DBF]" type="radio" id="radiopickup" name="radioselector" value="radiopickup" checked={delivery == false} onChange={handleChange} />
+                    <label htmlFor="radiopickup">Osobní vyzvednutí</label>
                   </div>
-                  <div className="p-2 w-full">
-                    <div className="relative my-2 mx-1">
-                      <label htmlFor="name" className="leading-7 font-semibold text-base text-gray-600">Jméno a příjmení</label>
-                      <input type="text" id="name" name="name" placeholder="Aa" className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>
-                    <div className="text-sm flex  text-[#957D5E]">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-                      </svg>Uveďte jméno osoby, která bude zásilku přebírat</div>
-                  </div>
-                  <div className="p-2 w-full">
-                    <div className="relative my-2 mx-1">
-                      <label htmlFor="street" className="leading-7 font-semibold text-base text-gray-600">Ulice a číslo popisné</label>
-                      <input type="text" id="street" name="street" placeholder="Putlerova 12" className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>
-                  </div>
-                  <div className="p-2 w-full">
-                    <div className="relative my-2 mx-1">
-                      <label htmlFor="city" className="leading-7 font-semibold text-base text-gray-600">Město</label>
-                      <input type="text" id="city" name="city" placeholder="Praha" className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>
-                  </div>
-                  <div className="p-2 w-1/2">
-                    <div className="relative my-2 mx-1">
-                      <label htmlFor="city" className="leading-7 font-semibold text-base text-gray-600">PSČ</label>
-                      <input type="text" id="psc" name="psc" placeholder="14000" className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>
-                  </div>
-                  <div className="p-2 w-full">
-                    <div className="relative my-2 mx-1">
-                      <div className="flex">
-                        <label htmlFor="phone2" className="leading-7 grow font-semibold text-base text-gray-600">Email</label>
-                        <span className="text-[#957D5E] font-bold text-sm">volitelné</span>
+                  {delivery == true ? (
+                    <>
+                      <div className="p-2 w-full">
+                        <div className="relative my-2 mx-1">
+                          <label htmlFor="name" className="leading-7 font-semibold text-base text-gray-600">Jméno a příjmení</label>
+                          <input type="text" id="name" name="name" placeholder="Aa" className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <div className="text-sm flex  text-[#957D5E]">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+                          </svg>Uveďte jméno osoby, která bude zásilku přebírat</div>
                       </div>
-                      <input type="tel" id="phone2" name="phone2" placeholder="+380 111 111 111" className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                      <div className="p-2 w-full">
+                        <div className="relative my-2 mx-1">
+                          <label htmlFor="street" className="leading-7 font-semibold text-base text-gray-600">Ulice a číslo popisné</label>
+                          <input type="text" id="street" name="street" placeholder="Putlerova 12" className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                      </div>
+                      <div className="p-2 w-full">
+                        <div className="relative my-2 mx-1">
+                          <label htmlFor="city" className="leading-7 font-semibold text-base text-gray-600">Město</label>
+                          <input type="text" id="city" name="city" placeholder="Praha" className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                      </div>
+                      <div className="p-2 w-1/2">
+                        <div className="relative my-2 mx-1">
+                          <label htmlFor="city" className="leading-7 font-semibold text-base text-gray-600">PSČ</label>
+                          <input type="text" id="psc" name="psc" placeholder="14000" className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                      </div>
+                      <div className="p-2 w-full">
+                        <div className="relative my-2 mx-1">
+                          <div className="flex">
+                            <label htmlFor="email2" className="leading-7 grow font-semibold text-base text-gray-600">Email</label>
+                            <span className="text-[#957D5E] font-bold text-sm">volitelné</span>
+                          </div>
+                          <input type="tel" id="email2" name="email2" placeholder="@" className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <div className="text-sm flex  text-[#957D5E]">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mx-1" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+                          </svg>Uveďte pokud se liší od telefonního čísla výše</div>
+                      </div>
+                    </>) : (
+                    <div className="p-2 w-full">
+                      <div className="relative my-2 mx-1 bg-[#0A9DBF] text-xl flex flex-col text-[#F8EBDB] font-semibold p-5 md:w-2/3 ">
+                        <span>Hala 13 v Pražské tržnici</span>
+                        <span>Bubenské nábřeží 306</span>
+                        <span>170 00 Praha 7</span>
+
+                        <a className="mt-5" href="tel:+420737597070">Mobil: 737 59 70 70</a>
+                        <a href="mailto:info@satnikpraha.cz">E-mail: info@satnikpraha.cz</a>
+                        <a href="https://goo.gl/maps/o6Z8qzkG6pnTRs7P8" target="_blank" rel="noreferrer">
+                          <button className="w-44 text-base mt-10 text-[#F8EBDB] border-0 py-2 px-3 focus:outline-none outline  outline-[#F8EBDB] rounded-full  hover:text-[#eb2f06] hover:bg-[#F8EBDB]  hover:outline-[#F8EBDB] font-semibold">Ukázat na mapě</button>
+                        </a>
+                      </div>
                     </div>
-                    <div className="text-sm flex  text-[#957D5E]">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mx-1" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-                      </svg>Uveďte pokud se liší od telefonního čísla výše</div>
-                  </div>
-                  <div className="p-2 my-10 ml-2 w-full">
-                    <button className="inline-flex items-center text-[#0A9DBF] border-0 py-4 px-6 focus:outline-none outline  outline-[#0A9DBF] rounded-full text-xl hover:bg-[#eb2f06] hover:text-white hover:outline-[#eb2f06]">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  )}
+                  <div className="p-2 my-10 mx-2 w-full md:w-1/2">
+                    <button className="inline-flex items-center w-full  text-[#0A9DBF] border-0 py-4 px-6 focus:outline-none outline  outline-[#0A9DBF] rounded-full text-xl hover:bg-[#eb2f06] hover:text-white hover:outline-[#eb2f06]">
+                      Pokračovat na výběr oblečení
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
-                      </svg>
-                      Pokračovat na výběr oblečení</button>
+                      </svg></button>
                   </div>
                 </div>
               </div>
