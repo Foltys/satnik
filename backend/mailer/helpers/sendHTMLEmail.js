d76c302cb94840a3ceca1ee7edd85f3ec6e40d9b
michal.palma <michal.palma@socialbakers.com>
2022-03-06 17:39:14 +0100

    [backend][mailing] dynamic html mailing - sendOrderConfirmation

---
+++ backend/mailer/helpers/sendHTMLEmail.js
@@ -0,0 +1,52 @@
+const hbs = require('nodemailer-express-handlebars')
+const fsna = require('fs')
+const nodemailer = require('nodemailer')
+const fs = fsna.promises
+
+async function getImagesAttachments (path) {
+	const files = await fs.readdir(path)
+	return files.filter(f => !['.', '..'].includes(f)).map(f => {
+		return {
+			filename: f,
+			cid: f,
+			path: [path, f].join('/'),
+		}
+	})
+}
+
+const transporter = nodemailer.createTransport({
+
+	// setting up gmail sender is best against antispam filters
+	// tutorial for setting up gmail account allowance:
+	// for better security with OAuth2 we need redirect page at our app
+	// https://www.geeksforgeeks.org/how-to-send-email-using-node-js/
+
+	service: 'gmail',
+	auth: {
+		user: 'prahasatnik',
+		pass: 'KwAEer8triU4qgU',
+	},
+})
+
+exports.default = async function (data, subject, lang, templatePath) {
+	const handlebarOptions = {
+		viewEngine: {
+			partialsDir: [templatePath, lang].join('/'),
+			defaultLayout: false,
+		},
+		viewPath: [templatePath, lang].join('/'),
+	}
+
+	transporter.use('compile', hbs(handlebarOptions))
+	const mailOptions = {
+		from: 'prahasatnik@gmail.com',
+		to: data.email,
+		subject: subject,
+		template: 'index',
+		attachments: await getImagesAttachments([templatePath, 'images'].join('/')),
+		context: {order: data},
+	}
+
+	const info = await transporter.sendMail(mailOptions)
+	return info
+}
--- backend/mailer/sendOrderConfirmation.js
+++ backend/mailer/sendOrderConfirmation.js
@@ -1,11 +1,10 @@
-const nodemailer = require('nodemailer')
-const hbs = require('nodemailer-express-handlebars')
 const path = require('path')
+const sendHTMLEmail = require('./helpers/sendHTMLEmail').default
 
 function normalize (order) {
 	order.persons.map((p) => {
 		p.requirements.map((r) => {
-			r.description.replace("\n", ', ')
+			r.description.replace('\n', ', ')
 			r.description.replace(/ +/g, ' ')
 		})
 	})
@@ -13,49 +12,24 @@ function normalize (order) {
 	order.isDeliveryPickup = order.delivery_type === 'pickup'
 }
 
-const transporter = nodemailer.createTransport({
-
-	// setting up gmail sender is best against antispam filters
-	// tutorial for setting up gmail account allowance:
-	// for better security with OAuth2 we need redirect page at our app
-	// https://www.geeksforgeeks.org/how-to-send-email-using-node-js/
-
-	service: 'gmail',
-	auth: {
-		user: 'prahasatnik',
-		pass: 'KwAEer8triU4qgU',
+const translations = {
+	subject: {
+		cs: 'Potvrzení objednavky ze satniku',
+		ua: 'Підтвердження замовлення з шафи',
 	},
-})
+}
 
 const sendOrderConfirm = async function (order) {
 	order = JSON.parse(JSON.stringify(order))
 	normalize(order)
 	const lang = order.lang === 'cs' ? 'cs' : 'ua'
 
-	const handlebarOptions = {
-		viewEngine: {
-			partialsDir: path.resolve(`./html/order_confirm/${lang}/`),
-			defaultLayout: false,
-		},
-		viewPath: path.resolve(`./html/order_confirm/${lang}/`),
-	}
-
-	transporter.use('compile', hbs(handlebarOptions))
-	const mailOptions = {
-		from: 'prahasatnik@gmail.com',
-		to: order.email,
-		subject: 'Nova objednavka ze satniku',
-		template: 'index',
-		attachments: [
-			{filename: 'image-1.png', cid: 'image-1.png', path: './html/order_confirm/images/image-1.png'},
-			{filename: 'image-2.png', cid: 'image-2.png', path: './html/order_confirm/images/image-2.png'},
-			{filename: 'image-3.png', cid: 'image-3.png', path: './html/order_confirm/images/image-3.png'},
-		],
-		context: {order},
-	}
-
-	const info = await transporter.sendMail(mailOptions)
-	return info
+	return sendHTMLEmail(
+		order,
+		translations.subject[lang],
+		lang,
+		path.resolve(`./html/order_confirm`),
+	)
 }
 
 exports.default = {sendOrderConfirm}
