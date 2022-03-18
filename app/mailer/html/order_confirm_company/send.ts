fac5e92352aa6fe6891d2dc07151bd826316f6ea
michal.palma <michal.palma@socialbakers.com>
2022-03-18 10:11:47 +0100

    fixed mail template path order_confirm_company

--- app/mailer/html/order_confirm_company/send.ts
+++ app/mailer/html/order_confirm_company/send.ts
@@ -11,7 +11,7 @@ export const send = async function (order: Order) {
 		order,
 		'Nova objednavka v satniku',
 		'cs',
-		path.join('./app/mailer/html/order_confirm'),
+		path.join('./app/mailer/html/order_confirm_company'),
 		path.join('./app/mailer/html/images'),
 	)
 }
