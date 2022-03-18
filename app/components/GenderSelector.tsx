55cf8158e9cda041ec044732f76c66e4536ac5c1
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-18 13:37:31 +0100

    gender size

--- app/components/GenderSelector.tsx
+++ app/components/GenderSelector.tsx
@@ -13,7 +13,7 @@ export default function GenderSelector({ translator, selectGender }: GenderSelec
 		<div className="flex space-x-2 mb-16 md:mb-12 text-gray-800">
 			<button
 				onClick={selectGender('woman')}
-				className="rounded-2xl group bg-white w-1/3 lg:w-1/4 h-80 p-2 border border-[#957D5E] flex flex-col hover:border-[#0A9DBF] hover:bg-[#F8EBDB] items-center"
+				className="rounded-2xl group bg-white w-1/3 h-80 p-2 border border-[#957D5E] flex flex-col hover:border-[#0A9DBF] hover:bg-[#F8EBDB] items-center"
 			>
 				<img src="woman.svg" alt="man" className="my-4 " />
 				<span className="text-2xl font-bold">{translator.translate('woman')}</span>
@@ -21,7 +21,7 @@ export default function GenderSelector({ translator, selectGender }: GenderSelec
 			</button>
 			<button
 				onClick={selectGender('man')}
-				className="rounded-2xl group bg-white w-1/3 lg:w-1/4 h-80 p-2 border border-[#957D5E] flex flex-col hover:border-[#0A9DBF] hover:bg-[#F8EBDB] items-center"
+				className="rounded-2xl group bg-white w-1/3  h-80 p-2 border border-[#957D5E] flex flex-col hover:border-[#0A9DBF] hover:bg-[#F8EBDB] items-center"
 			>
 				<img src="man.svg" alt="man" className="my-4 " />
 				<span className="text-2xl font-bold">{translator.translate('man')}</span>
@@ -29,7 +29,7 @@ export default function GenderSelector({ translator, selectGender }: GenderSelec
 			</button>
 			<button
 				onClick={selectGender('kid')}
-				className="rounded-2xl group bg-white w-1/3 lg:w-1/4 h-80 p-2 border border-[#957D5E] flex flex-col hover:border-[#0A9DBF] hover:bg-[#F8EBDB] items-center"
+				className="rounded-2xl group bg-white w-1/3 h-80 p-2 border border-[#957D5E] flex flex-col hover:border-[#0A9DBF] hover:bg-[#F8EBDB] items-center"
 			>
 				<img src="kid.svg" alt="man" className="my-4 " />
 				<span className="text-2xl font-bold">{translator.translate('kid')}</span>
