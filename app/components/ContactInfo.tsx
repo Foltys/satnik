b06298eebfb61945349ce7729dd83aa8b9f71125
Jakub Folejtar <jakub.folejtar@socialbakers.com>
2022-03-18 10:59:01 +0100

    require email

--- app/components/ContactInfo.tsx
+++ app/components/ContactInfo.tsx
@@ -77,6 +77,7 @@ export default function ContactInfo({ translator, handleChange, order }: Contact
 							name="email"
 							placeholder="@"
 							autoComplete='email'
+              required
 							className="w-full mt-1 bg-white bg-opacity-80 rounded-xl border border-[#957D5E] focus:border-white focus:bg-white focus:ring-2 focus:ring-[#0A9DBF] text-base outline-none placeholder:text-[#C6B49D] py-2 px-3 leading-8 transition-colors duration-200 ease-in-out invalid:border-red-500  valid:border-[#0A9DBF]"
 							defaultValue={order.email}
 						/>
