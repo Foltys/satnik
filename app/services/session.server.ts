6244ee03910dc467b1e120619d0932584f5e7d70
michal.palma <michal.palma@socialbakers.com>
2022-03-20 20:43:08 +0100

    auth tunning
    This reverts commit f9f3c2018c8da346aa3f87ca2b9e3557e6d6bb86.

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -9,6 +9,8 @@ export const sessionStorage = createCookieSessionStorage({
 		name: "_session", // use any name you want here
 		sameSite: "lax", // this helps with CSRF
 		path: "/", // remember to add this so the cookie will work in all routes
+		httpOnly: true, // for security reasons, make this cookie http only
+		secrets: config.get('session.secrets'), // replace this with an actual secret
 		secure: true
 	},
 });
