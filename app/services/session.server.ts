18dffdb514c30d58982136adae16826de8bf33f5
michal.palma <michal.palma@socialbakers.com>
2022-03-20 20:11:20 +0100

    auth tunning
    This reverts commit 429a72b85e588524bc2c03919eeaad3908b6d26f.

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,15 +1,18 @@
-import { createCookie, createMemorySessionStorage } from 'remix'
+import { createCookieSessionStorage } from 'remix'
 
 // please note that app is running from build/index.js
 process.env["NODE_CONFIG_DIR"] = __dirname + "/../config";
 const config = require('config')
 
-const sessionCookie = createCookie("__session", {
-	secrets: config.get('session.secrets'),
-	sameSite: "lax",
-})
-export const sessionStorage = createMemorySessionStorage({
-	cookie: sessionCookie,
-})
+export const sessionStorage = createCookieSessionStorage({
+	cookie: {
+		name: "_session", // use any name you want here
+		sameSite: "lax", // this helps with CSRF
+		path: "/", // remember to add this so the cookie will work in all routes
+		httpOnly: true, // for security reasons, make this cookie http only
+		secrets: config.get('session.secrets'), // replace this with an actual secret
+		secure: true
+	},
+});
 
 export const { getSession, commitSession, destroySession } = sessionStorage;
