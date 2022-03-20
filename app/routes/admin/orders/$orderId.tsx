1752234246a3fd27b612ea832b44fd2ca1f90f7a
michal.palma <michal.palma@socialbakers.com>
2022-03-20 18:33:16 +0100

    auth added also into routes/admin/orders/$orderId

--- app/routes/admin/orders/$orderId.tsx
+++ app/routes/admin/orders/$orderId.tsx
@@ -1,21 +1,31 @@
-import { json, useLoaderData } from 'remix'
+import { Order } from '@prisma/client'
 import type { LoaderFunction } from 'remix'
+import { useLoaderData } from 'remix'
+import { OAuth2Profile } from 'remix-auth-oauth2'
 
 import { db } from '~/db.server'
+import { authenticator } from '~/server/auth.server'
 
-export const loader: LoaderFunction = async ({ params }) => {
+type LoaderData = {
+	user: OAuth2Profile
+	order: Order
+}
+
+export const loader: LoaderFunction = async ({ request, params }) => {
 	const orderid: number = ~~params.orderId!
-	return json(
-		await db.order.findUnique({
-			where: {
-				id: orderid,
-			},
-		}),
-	)
+	const order = await db.order.findUnique({
+		where: {
+			id: orderid,
+		},
+	})
+	const user = await authenticator.isAuthenticated(request, {
+		failureRedirect: "/login",
+	}) as OAuth2Profile
+	return {user, order}
 }
 
 export default function ProductCategory() {
-	const order = useLoaderData()
+	const {user, order} = useLoaderData()
 	return (
 		<div>
 			<p>{order.fullname}</p>
