429a72b85e588524bc2c03919eeaad3908b6d26f
michal.palma <michal.palma@socialbakers.com>
2022-03-20 19:43:46 +0100

    auth tunning

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,18 +1,15 @@
-import { createCookieSessionStorage } from 'remix'
+import { createCookie, createMemorySessionStorage } from 'remix'
 
 // please note that app is running from build/index.js
 process.env["NODE_CONFIG_DIR"] = __dirname + "/../config";
 const config = require('config')
 
-export const sessionStorage = createCookieSessionStorage({
-	cookie: {
-		name: "_session", // use any name you want here
-		sameSite: "lax", // this helps with CSRF
-		path: "/", // remember to add this so the cookie will work in all routes
-		httpOnly: true, // for security reasons, make this cookie http only
-		secrets: config.get('session.secrets'), // replace this with an actual secret
-		secure: true
-	},
-});
+const sessionCookie = createCookie("__session", {
+	secrets: config.get('session.secrets'),
+	sameSite: "lax",
+})
+export const sessionStorage = createMemorySessionStorage({
+	cookie: sessionCookie,
+})
 
 export const { getSession, commitSession, destroySession } = sessionStorage;
