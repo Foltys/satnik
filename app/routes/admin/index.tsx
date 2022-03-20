3aecbad0b3c425e3f9d9c145c57149261ae5a9cb
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-20 14:00:51 +0100

    admin shaping up

--- app/routes/admin/index.tsx
+++ app/routes/admin/index.tsx
@@ -3,7 +3,7 @@ import { json, Form } from 'remix'
 import { useLoaderData, Link } from 'remix'
 import { db } from '~/db.server'
 import type { Order } from '@prisma/client'
-
+import { Fragment } from 'react'
 
 type LoaderData = {
 	orderListItems: Array<Order>
@@ -12,7 +12,6 @@ type LoaderData = {
 export const loader: LoaderFunction = async ({ request }) => {
 	const orderListItems = await db.order.findMany({
 		take: 5,
-		//select: { id: true, fullname: true },
 		//where: { lang: "ua" },
 		orderBy: { created_at: 'desc' },
 	})
@@ -23,6 +22,20 @@ export const loader: LoaderFunction = async ({ request }) => {
 	return json(data)
 }
 
+// fullname: { type: 'string' },
+// phone: { type: 'string' },
+// email: { type: 'string' },
+// delivery_type: { const: 'delivery' },
+// delivery_fullname: { type: 'string' },
+// delivery_street: { type: 'string' },
+// delivery_city: { type: 'string' },
+// delivery_zip: { type: 'string' },
+// delivery_phone: { type: 'string' },
+// persons: { type: 'array', items: personSchema },
+// state: { enum: ['open'] },
+// lang: { enum: ['ua', 'cs'] },
+// created_at: {},
+// updated_at: {},
 export default function OrdersScreen() {
 	const data = useLoaderData<LoaderData>()
 
@@ -37,9 +50,23 @@ export default function OrdersScreen() {
 			<div>Stav objednávky</div>
 			<div>Možnosti objednávky</div>
 			{data.orderListItems.map((order) => (
-				<div key={order.id}>
-					<Link to={`${order.id}`}>{order.id}</Link>
-				</div>
+				<Fragment key={order.id}>
+					<div>
+						<Link to={`${order.id}`}>{order.id}</Link>
+					</div>
+
+					<div>{order.fullname}</div>
+					<div>
+						{order.delivery_city},{order.delivery_street}
+					</div>
+					<div>ASAP</div>
+					<div>{order.delivery_type}</div>
+					<div>{order.email}</div>
+					<div>Nová</div>
+					<div>
+						<button>Zpracovat</button>
+					</div>
+				</Fragment>
 			))}
 		</div>
 	)
