4a7a8a369e7df264ccce370f5b45af3b626501be
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-20 17:12:54 +0100

    working v0

--- app/routes/admin/index.tsx
+++ app/routes/admin/index.tsx
@@ -21,22 +21,39 @@ export const loader: LoaderFunction = async ({ request }) => {
 	return json(data)
 }
 
-export const action: ActionFunction = async ({ request }) => {
-	const form = await request.formData()
-	const orderId: number = Number(form.get('process'))
-	//console.log(id)
-	const updateOrder = await db.order.update({
+async function updateOrder(orderId: number, state: string) {
+	//console.log('updateOrder', orderId, state)
+	await db.order.update({
 		where: { id: orderId },
 		data: {
-		  state: 'processed',
+			state: state,
 		},
-	  })
-	//const orderToUpdate = await db.order.findUnique({
-	//	where: { id: orderId },
-	//})
-	//console.log(orderToUpdate)
+	})
+}
 
-	return null
+export const action: ActionFunction = async ({ request }) => {
+	const form = await request.formData()
+
+	const orderId: number = Number(form.get('updateid'))
+	//console.log(id)
+	let action = form.get('action')
+	switch (action) {
+		case 'finish': {
+			updateOrder(orderId, 'done')
+			return null
+		}
+		case 'process': {
+			updateOrder(orderId, 'process')
+			return null
+		}
+		case 'revert': {
+			updateOrder(orderId, 'open')
+			return null
+		}
+		default: {
+			throw new Error('Unexpected action')
+		}
+	}
 }
 
 // sem jenom línej
@@ -58,38 +75,49 @@ export default function OrdersScreen() {
 	const data = useLoaderData<LoaderData>()
 
 	return (
-		<Form method="post">
-			<div className="grid grid-cols-8 gap-4 text-gray-800 container mx-auto pt-6">
-				<div className="font-semibold pb-12">Číslo obj.</div>
-				<div className="font-semibold pb-12">Jméno</div>
-				<div className="font-semibold pb-12">Místo doručení</div>
-				<div className="font-semibold pb-12">Termín doručení</div>
-				<div className="font-semibold pb-12">Způsob dopravy</div>
-				<div className="font-semibold pb-12">Kontakt na objednávajícího</div>
-				<div className="font-semibold pb-12">Stav objednávky</div>
-				<div className="font-semibold pb-12">Možnosti objednávky</div>
-				{data.orderListItems.map((order) => (
-					<Fragment key={order.id}>
-						<div className="text-[#0A9DBF] font-semibold underline">
-							<Link to={`${order.id}`}>{order.id}</Link>
-						</div>
+		<div className="grid grid-cols-8 gap-4 text-gray-800 container mx-auto pt-6">
+			<div className="font-semibold pb-12">Číslo obj.</div>
+			<div className="font-semibold pb-12">Jméno</div>
+			<div className="font-semibold pb-12">Místo doručení</div>
+			<div className="font-semibold pb-12">Termín doručení</div>
+			<div className="font-semibold pb-12">Způsob dopravy</div>
+			<div className="font-semibold pb-12">Kontakt na objednávajícího</div>
+			<div className="font-semibold pb-12">Stav objednávky</div>
+			<div className="font-semibold pb-12">Možnosti objednávky</div>
+			{data.orderListItems.map((order) => (
+				<Fragment key={order.id}>
+					<div className="text-[#0A9DBF] font-semibold underline">
+						<Link to={`${order.id}`}>{order.id}</Link>
+					</div>
 
-						<div>{order.fullname}</div>
+					<div>{order.fullname}</div>
+					<div>
+						{order.delivery_city},{order.delivery_street}
+					</div>
+					<div>ASAP</div>
+					<div>{order.delivery_type}</div>
+					<div>{order.email}</div>
+					<div>{order.state}</div>
+					<Form method="post">
 						<div>
-							{order.delivery_city},{order.delivery_street}
+							{order.state === 'open' ? (
+								<button name="action" value="process">
+									Začít vyřizovat
+								</button>
+							) : order.state != 'done' ? (
+								<button name="action" value="finish">
+									Označit jako vyřízené
+								</button>
+							) : (
+								<button name="action" value="revert">
+									Vrátit zpět
+								</button>
+							)}
 						</div>
-						<div>ASAP</div>
-						<div>{order.delivery_type}</div>
-						<div>{order.email}</div>
-						<div>{order.state}</div>
-						<div>
-							<button name="process" value={order.id}>
-								Zpracovat
-							</button>
-						</div>
-					</Fragment>
-				))}
-			</div>
-		</Form>
+						<input type="hidden" name="updateid" value={order.id} />
+					</Form>
+				</Fragment>
+			))}
+		</div>
 	)
 }
