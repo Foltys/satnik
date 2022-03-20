eef930be83e07aa37f3e879cb4a578c4f782b404
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-20 15:34:58 +0100

    fucking typescript, I hate you

--- app/routes/admin/index.tsx
+++ app/routes/admin/index.tsx
@@ -1,6 +1,5 @@
-import type { LoaderFunction } from 'remix'
-import { json, Form } from 'remix'
-import { useLoaderData, Link } from 'remix'
+import type { LoaderFunction, ActionFunction } from 'remix'
+import { useLoaderData, Link, json, Form, useTransition } from 'remix'
 import { db } from '~/db.server'
 import type { Order } from '@prisma/client'
 import { Fragment } from 'react'
@@ -22,6 +21,17 @@ export const loader: LoaderFunction = async ({ request }) => {
 	return json(data)
 }
 
+export const action: ActionFunction = async ({ request }) => {
+	const form = await request.formData()
+	const orderId: number = Number(form.get('process'))
+	//console.log(id)
+	const orderToUpdate = await db.order.findUnique({
+		where: { id: orderId },
+	})
+	console.log(orderToUpdate)
+	return null
+}
+
 // sem jenom línej
 //fullname: { type: 'string' },
 // phone: { type: 'string' },
@@ -41,34 +51,38 @@ export default function OrdersScreen() {
 	const data = useLoaderData<LoaderData>()
 
 	return (
-		<div className="grid grid-cols-8 gap-4 text-gray-800 container mx-auto pt-6">
-			<div className="font-semibold pb-12">Číslo obj.</div>
-			<div className="font-semibold pb-12">Jméno</div>
-			<div className="font-semibold pb-12">Místo doručení</div>
-			<div className="font-semibold pb-12">Termín doručení</div>
-			<div className="font-semibold pb-12">Způsob dopravy</div>
-			<div className="font-semibold pb-12">Kontakt na objednávajícího</div>
-			<div className="font-semibold pb-12">Stav objednávky</div>
-			<div className="font-semibold pb-12">Možnosti objednávky</div>
-			{data.orderListItems.map((order) => (
-				<Fragment key={order.id}>
-					<div className='text-[#0A9DBF] font-semibold underline'>
-						<Link to={`${order.id}`}>{order.id}</Link>
-					</div>
+		<Form method="post">
+			<div className="grid grid-cols-8 gap-4 text-gray-800 container mx-auto pt-6">
+				<div className="font-semibold pb-12">Číslo obj.</div>
+				<div className="font-semibold pb-12">Jméno</div>
+				<div className="font-semibold pb-12">Místo doručení</div>
+				<div className="font-semibold pb-12">Termín doručení</div>
+				<div className="font-semibold pb-12">Způsob dopravy</div>
+				<div className="font-semibold pb-12">Kontakt na objednávajícího</div>
+				<div className="font-semibold pb-12">Stav objednávky</div>
+				<div className="font-semibold pb-12">Možnosti objednávky</div>
+				{data.orderListItems.map((order) => (
+					<Fragment key={order.id}>
+						<div className="text-[#0A9DBF] font-semibold underline">
+							<Link to={`${order.id}`}>{order.id}</Link>
+						</div>
 
-					<div>{order.fullname}</div>
-					<div>
-						{order.delivery_city},{order.delivery_street}
-					</div>
-					<div>ASAP</div>
-					<div>{order.delivery_type}</div>
-					<div>{order.email}</div>
-					<div>{order.state}</div>
-					<div>
-						<button>Zpracovat</button>
-					</div>
-				</Fragment>
-			))}
-		</div>
+						<div>{order.fullname}</div>
+						<div>
+							{order.delivery_city},{order.delivery_street}
+						</div>
+						<div>ASAP</div>
+						<div>{order.delivery_type}</div>
+						<div>{order.email}</div>
+						<div>{order.state}</div>
+						<div>
+							<button name="process" value={order.id}>
+								Zpracovat
+							</button>
+						</div>
+					</Fragment>
+				))}
+			</div>
+		</Form>
 	)
 }
