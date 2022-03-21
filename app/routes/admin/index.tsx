ac1e71cbde2311f8240f3afce4ea386b1e4332b4
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-21 17:30:26 +0100

    admin login UI v 0

--- app/routes/admin/index.tsx
+++ app/routes/admin/index.tsx
@@ -108,31 +108,46 @@ export default function OrdersScreen() {
 
 	return (
 		<>
-			{/* tohle ochcává JIT tailwindu abych ty barvy mohl použít ve funkci */}
 			{data.user && (
-				<>
-					<ul>
-						<li>
-							<strong>displayName:</strong> {data.user.displayName}
-						</li>
-						<li>
-							<strong>provider:</strong> {data.user.provider}
-						</li>
-						<li>
-							<strong>emails:</strong> {JSON.stringify(data.user.emails)}
-						</li>
-						{data.user.photos &&
-							data.user.photos.map((photo: any) => (
-								<li key={data.user?.id}>
-									<img src={photo.value} alt="profile" />
-								</li>
-							))}
-					</ul>
-					<Form action="/logout" method="post">
-						<button>Logout</button>
-					</Form>
-				</>
+				<header className="text-gray-600 body-font">
+					<div className="container mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
+						<div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+							{data.user.photos &&
+								data.user.photos.map((photo: any) => (
+									<img
+										key={data.user?.id}
+										src={photo.value}
+										alt={JSON.stringify(data?.user?.emails) || 'profile'}
+										className="w-10 h-10 text-white border-[#0A9DBF] rounded-full"
+									/>
+								))}
+							<span className="ml-3 text-xl">{data.user.displayName}</span>
+						</div>
+						<nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 	flex flex-wrap items-center text-base justify-center">
+							<a className="mr-5 hover:text-gray-900 hidden" href="/">
+								maybe some links
+							</a>
+						</nav>
+						<Form action="/logout" method="post">
+							<button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+								Logout
+								<svg
+									fill="none"
+									stroke="currentColor"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									className="w-4 h-4 ml-1"
+									viewBox="0 0 24 24"
+								>
+									<path d="M5 12h14M12 5l7 7-7 7"></path>
+								</svg>
+							</button>
+						</Form>
+					</div>
+				</header>
 			)}
+			{/* tohle ochcává JIT tailwindu abych ty barvy mohl použít ve funkci */}
 			<div className="border border-black hidden"></div>
 			<div className="border border-blue-500 hidden"></div>
 			<div className="border border-red-500 hidden"></div>
@@ -147,9 +162,7 @@ export default function OrdersScreen() {
 				<div className="font-semibold pb-12">Možnosti objednávky</div>
 				{data.orderListItems.map((order) => (
 					<Fragment key={order.id}>
-						<div className="text-[#0A9DBF] font-semibold ">
-							{order.id}
-						</div>
+						<div className="text-[#0A9DBF] font-semibold ">{order.id}</div>
 
 						<div>{order.fullname}</div>
 						{order.delivery_type === 'pickup' ? (
