ba1a967b16f86c7fd46e737a021c8cbcdee19720
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-21 22:32:04 +0100

    tak zas zejtra

--- app/routes/admin/$orderId.tsx
+++ app/routes/admin/$orderId.tsx
@@ -33,8 +33,53 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 export default function ProductCategory() {
 	const { user, order } = useLoaderData()
 	return (
-		<div>
-			<p>{order.fullname}</p>
+		<div className="flex flex-col">
+			<p className="text-3xl font-bold">{order.id}</p>
+			<p className="text-sm font-semibold text-[#C6B49D] py-2">Vytvořeno: {order.created_at}</p>
+			<div className="flex flex-col">
+				{order.persons.map((person: any) => (
+					<div key={person.id}>
+						<div className="flex flex-row gap-6">
+							<div className="flex flex-col">
+								<p className="font-semibold text-lg text-[#0A9DBF]">{person.fullname}</p>
+								<p>
+									{person.sex}, {person.age}
+								</p>
+							</div>
+							<div className='flex justify-center align-center'>
+								<div className=" p-4 rounded-xl bg-[#C6B49D]">{person.clothing_size}</div>
+							</div>
+							<div className='flex justify-center align-center'>
+								<div className=" p-4 rounded-xl bg-[#C6B49D]">{person.shoe_size}</div>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	)
 }
+
+// [key: string]: any
+// sex: 'man' | 'woman'
+// adult: boolean
+// fullname: string
+// age: string
+// clothing_size: string
+// shoe_size: string
+// requirements: Requirement[]
+//----
+// fullname: string
+// phone: string
+// email: string
+// delivery_type: 'delivery' | 'pickup'
+// delivery_fullname?: string
+// delivery_street?: string
+// delivery_city?: string
+// delivery_zip?: string
+// delivery_phone?: string
+// persons: Person[]
+// state: 'open'
+// lang: 'ua' | 'cs'
+// created_at: {}
+// updated_at: {}
