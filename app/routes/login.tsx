c803683a8d9c5e725bc7cc035db03bae1a8a62b9
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-24 14:46:01 +0100

    login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -2,20 +2,31 @@ import { Form } from 'remix'
 import { SocialsProvider } from 'remix-auth-socials'
 
 interface SocialButtonProps {
-	provider: SocialsProvider,
+	provider: SocialsProvider
 	label: string
 }
 
 const SocialButton: React.FC<SocialButtonProps> = ({ provider, label }) => (
 	<Form action={`/auth/${provider}`} method="post">
-		<button>{label}</button>
+		<button className=" font-bold text-lg text-blue">{label}</button>
 	</Form>
-);
+)
 
 export default function Login() {
 	return (
-		<>
-			<SocialButton provider={SocialsProvider.GOOGLE} label="Login with Google" />
-		</>
-	);
+		<div className=" bg-blue w-full h-screen flex flex-col justify-center items-center">
+			<div className="w-full flex justify-center">
+				<img src="/satnik-beige.svg" alt="satnik logo" className="h-48 w-48" />
+			</div>
+			<div className="w-full h-screen flex justify-center items-center">
+				<div className="flex flex-col justify-center items-center">
+					<div className="w-full  text-5xl font-bold text-light flex justify-center pb-12">Přihlásit se do Šatníku</div>
+					<div className="bg-white w-80 h-16 rounded-3xl flex flex-row items-center ">
+						<img src="/googlelogo.svg" alt="google login logo" className=" w-8 h-8 my-2 mx-8 font-bold" />
+						<SocialButton provider={SocialsProvider.GOOGLE} label="Login with Google" />
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,15 +1,16 @@
 import { createCookieSessionStorage } from 'remix'
+import type { SessionStorage } from 'remix'
 import config from 'config'
 
-export const sessionStorage = createCookieSessionStorage({
+export const sessionStorage: SessionStorage = createCookieSessionStorage({
 	cookie: {
-		name: "_session",
-		sameSite: "lax",
-		path: "/",
+		name: '_session',
+		sameSite: 'lax',
+		path: '/',
 		httpOnly: true,
 		secrets: config.get('session.secrets') as string[],
-		secure: false
+		secure: false,
 	},
-});
+})
 
-export const { getSession } = sessionStorage;
+export const { getSession } = sessionStorage
