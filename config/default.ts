4857baf3df8fba061b930482771f8fb3f55de657
michal.palma <michal.palma@socialbakers.com>
2022-03-30 15:19:59 +0200

    confidential strings removed from configs into .env

---
+++ config/default.ts
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export default {
+	'hostname': 'http://localhost:3000',
+	'publicPath': '/build/',
+	'session': {
+		'secrets': (process.env.SESSION_SECRETS as string).split(',').map(s => s.trim()),
+	},
+	'email': {
+		'companyAddress': 'prahasatnik@gmail.com',
+		'provider': {
+			'service': 'gmail',
+			'auth': {
+				'user': 'prahasatnik',
+				'pass': process.env.EMAIL_PROVIDER_AUTH_PASSWORD as string,
+			},
+		},
+	},
+	'auth': {
+		'enabled': true,
+		'strategies': {
+			'google': {
+				'clientID': process.env.AUTH_GOOGLE_CLIENTID as string,
+				'clientSecret': process.env.AUTH_GOOGLE_CLIENTSECRET as string,
+				'allowedAccounts': [
+					'prahasatnik@gmail.com',
+					'michal.palma@gmail.com',
+					'adam.sobotka@emplifi.io',
+					'folejtar.jakub@gmail.com',
+				],
+			},
+		},
+	},
+}
